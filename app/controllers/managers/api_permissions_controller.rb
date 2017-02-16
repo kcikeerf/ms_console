e038f174c59c14017aff2aa29ab23f2b0b5520d0
sunsc <sunsc@qidianbj.com>
2017-02-16 16:50:35 +0800

    update

--- app/controllers/managers/api_permissions_controller.rb
+++ app/controllers/managers/api_permissions_controller.rb
@@ -1,21 +1,21 @@
-class Managers::PermissionsController < ApplicationController
-
+class Managers::ApiPermissionsController < ApplicationController
   respond_to :json, :html
 
   layout 'manager_crud'
 
+  before_action :set_role
   before_action :set_permission, only: [:edit, :update]
-  before_action :set_role, only: [:list]
     # skip_before_action :authenticate_person!
     # before_action :authenticate_manager
 
   def index
-    @permissions = Permission.page(params[:page]).per(params[:rows])
+    #@data = {name: '权限', path: '/managers/permissions'}
+    @permissions = ApiPermission.page(params[:page]).per(params[:rows])
     respond_with({rows: @permissions, total: @permissions.total_count}) 
   end
 
   def create
-    @permission = Permission.new(permission_params)
+    @permission = ApiPermission.new(permission_params)
     render json: response_json_by_obj(@permission.save, @permission)
   end
 
@@ -25,12 +25,12 @@ class Managers::PermissionsController < ApplicationController
   end
 
   def destroy_all
-    Permission.destroy(params[:id])
+    ApiPermission.destroy(params[:id])
     respond_with(@permission)
   end
 
   def list
-    permissions_data = @role.blank?? Permission : @role.permissions
+    permissions_data = @role.blank?? ApiPermission : @role.api_permissions
     result = permissions_data.select(:id,:name).map{|item|
       {
         :id => item.id,
@@ -47,10 +47,10 @@ class Managers::PermissionsController < ApplicationController
     end
 
     def set_permission
-      @permission = Permission.where(id: params[:id]).first
+      @permission = ApiPermission.find(params[:id])
     end
 
     def permission_params
-      params.permit(:id, :name, :subject_class, :operation, :description, :role_id)
+      params.permit(:authenticity_token, :id, :name, :method, :path, :description, :role_id)
     end
 end
