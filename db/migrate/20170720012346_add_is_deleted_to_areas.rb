9c7e2fd5cf1c1e5d45472893d4fae5c81bda125d
zuo1234 <zuo1234@mopaas.com>
2017-07-21 15:41:02 +0800

    rename migrate

--- db/migrate/20170720012346_add_is_deleted_to_areas.rb
+++ db/migrate/20170720012346_add_is_deleted_to_areas.rb
@@ -1,4 +1,4 @@
-class AddDeleteFlagToAreas < ActiveRecord::Migration
+class AddIsDeletedToAreas < ActiveRecord::Migration
   def change
     add_column :areas, :is_deleted, :boolean, default: false
   end
